0166224dd911dd840c62680289c3dfccbc3a440c
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-04-18 20:02:13 -0300

    Criação de elementos HTML no JS

--- javaScript/index.js
+++ javaScript/index.js
@@ -5,3 +5,16 @@ for (var i = 0; i < 6; i++) {
 }
 
 console.log(numerosSorteados);
+
+var div = document.createElement("div");
+
+var div = document.body;
+
+
+for (var i = 0; i < 12; i++) {
+	var botao = document.createElement('button');
+	div.appendChild(botao);
+}
+
+console.log(div);
+console.log(botao);
