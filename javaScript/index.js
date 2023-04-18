6a9ab1b5002175e32ed8cc01e1b6c2364bea1fcf
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-04-18 20:22:13 -0300

    Botões com valor sorteado

--- javaScript/index.js
+++ javaScript/index.js
@@ -6,13 +6,13 @@ for (var i = 0; i < 6; i++) {
 
 console.log(numerosSorteados);
 
-var div = document.createElement("div");
-
 var div = document.body;
-
 
 for (var i = 0; i < 12; i++) {
 	var botao = document.createElement('button');
+	var numeroSorteadoBotao = botao.value = Math.round(Math.random() * 100);
+	var valor  = document.createTextNode(numeroSorteadoBotao);
+	botao.appendChild(valor);
 	div.appendChild(botao);
 }
 
