7e5ea88ab369b2f82ebc10dd947bb8f91b1226e3
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-04-19 09:15:26 -0300

    Lógica para verificar acertos

--- javaScript/index.js
+++ javaScript/index.js
@@ -2,6 +2,8 @@ var numerosSorteados = [];
 var numerosDigitados = [];
 var contadorNumerosSorteados = 0;
 var contadorNumerosDigitados = 0;
+var contadorAcertos = 0;
+var verificouAcertos = false;
 
 while (numerosSorteados.length < 6) {
 	var valorSorteado = Math.round(Math.random() * 100);
@@ -17,10 +19,27 @@ function armazenaValor() {
 	var campoValorDigitado = document.getElementById("entrada");
 	var valorDigitado = parseInt(campoValorDigitado.value);
 
-	if (valorDigitado >= 10 && valorDigitado <= 30) {
+	if (valorDigitado >= 10 && valorDigitado <= 30 
+		&& contadorNumerosDigitados < 6) {
 		numerosDigitados[contadorNumerosDigitados] = valorDigitado;
 		contadorNumerosDigitados++;
 		console.log("Valor adicionado!");
 		console.log(numerosDigitados);
 	}
+
+	if (contadorNumerosDigitados == 6 && verificouAcertos == false) {
+		verificouAcertos = true;
+		comparaNumerosDigitadosComSorteados();
+	}
+}
+
+function comparaNumerosDigitadosComSorteados() {
+	for (var i = 0; i < 6; i++) {
+		for (var j = 0; j < 6; j++) {
+			if (numerosSorteados[i] == numerosDigitados[j]) {
+				contadorAcertos++;
+			}
+		}
+	}
+	console.log("Acertos = " + contadorAcertos);
 }
