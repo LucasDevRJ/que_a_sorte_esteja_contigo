fbf3cf16e77fd736edc99b8b2802f9d55767610e
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-04-19 08:52:59 -0300

    Função para armazenar valor no vetor

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,13 +1,26 @@
 var numerosSorteados = [];
 var numerosDigitados = [];
 var contadorNumerosSorteados = 0;
+var contadorNumerosDigitados = 0;
 
 while (numerosSorteados.length < 6) {
 	var valorSorteado = Math.round(Math.random() * 100);
-	if (valorSorteado >= 10 && valorSorteado <= 20) {
+	if (valorSorteado >= 10 && valorSorteado <= 30) {
 		numerosSorteados[contadorNumerosSorteados] = valorSorteado;
 		contadorNumerosSorteados++;
 	}
 }
 
 console.log(numerosSorteados);
+
+function armazenaValor() {
+	var campoValorDigitado = document.getElementById("entrada");
+	var valorDigitado = parseInt(campoValorDigitado.value);
+
+	if (valorDigitado >= 10 && valorDigitado <= 30) {
+		numerosDigitados[contadorNumerosDigitados] = valorDigitado;
+		contadorNumerosDigitados++;
+		console.log("Valor adicionado!");
+		console.log(numerosDigitados);
+	}
+}
