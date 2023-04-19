133d51a64bec737b3a56fcd05ecb4e4f4d6b1673
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-04-18 21:43:21 -0300

    Adição dos números sorteados

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,4 +1,5 @@
 var numerosSorteados = [];
+var botoesSorteados = [];
 
 for (var i = 0; i < 6; i++) {
 	numerosSorteados[i] = Math.round(Math.random() * 100);
@@ -6,35 +7,27 @@ for (var i = 0; i < 6; i++) {
 
 var div = document.body;
 
-for (var i = 0; i < 6; i++) {
-	if (i < numerosSorteados.length) {
-		var botao = document.createElement('button');
-		var numeroSorteadoBotao = botao.value = numerosSorteados[i];
+for (var i = 0; i < 12; i++) {
+	if (i < 6) {
+		botoesSorteados[i] = numerosSorteados[i];
+	} else {
+		botoesSorteados[i] = Math.round(Math.random() * 100);
+	}
+}	
+
+botoesSorteados.sort();
+
+for (var i = 0; i < 12; i++) {
+	var botao = document.createElement('button');
+	if (i < 6) {
+		var numeroSorteadoBotao = botao.value = botoesSorteados[i];
+		var valor  = document.createTextNode(numeroSorteadoBotao);
+		botao.appendChild(valor);
+		div.appendChild(botao);
+	} else {
+		var numeroSorteadoBotao = botao.value = Math.round(Math.random() * 100);
 		var valor  = document.createTextNode(numeroSorteadoBotao);
 		botao.appendChild(valor);
 		div.appendChild(botao);
 	}
 }
-
-for (var i = 6; i < 12; i++) {
-	var botao = document.createElement('button');
-	var numeroSorteadoBotao = botao.value = Math.round(Math.random() * 100);
-	var valor  = document.createTextNode(numeroSorteadoBotao);
-	botao.appendChild(valor);
-	div.appendChild(botao);
-}
-
-console.log(numerosSorteados);
-
-/*
-for (var i = 0; i < 12; i++) {
-	var botao = document.createElement('button');
-	var numeroSorteadoBotao = botao.value = Math.round(Math.random() * 100);
-	var valor  = document.createTextNode(numeroSorteadoBotao);
-	botao.appendChild(valor);
-	div.appendChild(botao);
-}
-
-console.log(div);
-console.log(botao);
-*/
