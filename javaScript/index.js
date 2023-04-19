0750dd27917cea028d59fb764033455c83a87232
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-04-18 21:31:44 -0300

    Lógica dos valores nos botões concluidas

--- javaScript/index.js
+++ javaScript/index.js
@@ -4,10 +4,29 @@ for (var i = 0; i < 6; i++) {
 	numerosSorteados[i] = Math.round(Math.random() * 100);
 }
 
-console.log(numerosSorteados);
-
 var div = document.body;
 
+for (var i = 0; i < 6; i++) {
+	if (i < numerosSorteados.length) {
+		var botao = document.createElement('button');
+		var numeroSorteadoBotao = botao.value = numerosSorteados[i];
+		var valor  = document.createTextNode(numeroSorteadoBotao);
+		botao.appendChild(valor);
+		div.appendChild(botao);
+	}
+}
+
+for (var i = 6; i < 12; i++) {
+	var botao = document.createElement('button');
+	var numeroSorteadoBotao = botao.value = Math.round(Math.random() * 100);
+	var valor  = document.createTextNode(numeroSorteadoBotao);
+	botao.appendChild(valor);
+	div.appendChild(botao);
+}
+
+console.log(numerosSorteados);
+
+/*
 for (var i = 0; i < 12; i++) {
 	var botao = document.createElement('button');
 	var numeroSorteadoBotao = botao.value = Math.round(Math.random() * 100);
@@ -18,3 +37,4 @@ for (var i = 0; i < 12; i++) {
 
 console.log(div);
 console.log(botao);
+*/
