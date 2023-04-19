f2bd54c790d9346c00e74d5df3aa7dbcbb724ee9
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-04-18 22:58:08 -0300

    Mais Arrays

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,30 +1,12 @@
-var botao1 = document.getElementById("botao1");
-var botao2 = document.getElementById("botao2");
-var botao3 = document.getElementById("botao3");
-var botao4 = document.getElementById("botao4");
-var botao5 = document.getElementById("botao5");
-var botao6 = document.getElementById("botao6");
-var botao7 = document.getElementById("botao7");
-var botao8 = document.getElementById("botao8");
-var botao9 = document.getElementById("botao9");
-var botao10 = document.getElementById("botao10");
-var botao11 = document.getElementById("botao11");
-var botao12 = document.getElementById("botao12");
-
-var botaoValor = botao1.value = Math.round(Math.random() * 100);
-
-botaoValor.innerHTML = botaoValor;
-
-console.log(botaoValor);
-
-/*
 var numerosSorteados = [];
 var botoesSorteados = [];
-var botoesSelecionados = [];
+var botoesEscolhidos = [];
 
 for (var i = 0; i < 6; i++) {
 	numerosSorteados[i] = Math.round(Math.random() * 100);
 }
+
+console.log(numerosSorteados);
 
 var div = document.body;
 
@@ -38,9 +20,10 @@ for (var i = 0; i < 12; i++) {
 
 botoesSorteados.sort();
 
+console.log(botoesSorteados);
+
 for (var i = 0; i < 12; i++) {
 	var botao = document.createElement('button');
-	botoesSelecionados[i] = botao;
 	if (i < 6) {
 		var numeroSorteadoBotao = botao.value = botoesSorteados[i];
 		var valor  = document.createTextNode(numeroSorteadoBotao);
@@ -53,11 +36,9 @@ for (var i = 0; i < 12; i++) {
 		div.appendChild(botao);
 	}
 
-	botoesSelecionados[i].onclick = function armazenaValor() {
-		botoesSelecionados[i] = numeroSorteadoBotao;
-		console.log(botoesSelecionados[i]);
-	}
+	botoesEscolhidos[i] = numeroSorteadoBotao;
 }
 
-console.log(botoesSelecionados);
-*/
+console.log(botoesEscolhidos);
+
+botoesEscolhidos.onclick = function 
